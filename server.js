96bfebcb7959a034843037134fad4abb7a3e6d2c
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-11-05 16:36:08 -0400

    Added libraries, success page

--- server.js
+++ server.js
@@ -2,15 +2,32 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const bodyparser = require('body-parser')
+const session = require('express-session')
+const { v4: uuidv4 } = require('uuid')
 
 //Set ejs engine
 app.set('view engine', 'ejs')
 
+//Setup body-parser
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
+
+//Setup express session
+app.use(session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true
+}))
+
 //Home router
 app.get('/', (req, res) => {
-    //Render the response
-    //title from base.ejs
     res.render('base', { title: 'Login System' })
+})
+
+//Login successful router
+app.get('/success', (req, res) => {
+    res.render('success', { title: 'Login Successful!' })
 })
 
 //Listen for server connection @ port variable
