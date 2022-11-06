22fb36ad78e118953abb1a77be416a82cf3d6955
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-11-06 12:11:24 -0500

    login/logout features successful

--- router.js
+++ router.js
@@ -14,15 +14,38 @@ const admin = {
 
 //User login
 //If user matches admin credentials, redirect to dashboard
-//Else end the response and print error
+//User's email becomes the displayed Username
+//Else, end the response and print error
 router.post('/login', (req, res) => {
-    if(req.body.email === admin.email && req.body.password === admin.password){
+    if(req.body.email == admin.email && req.body.password == admin.password){
         req.session.user = req.body.email
-        // res.redirect('/dashboard')
-        res.end('Login successful')
+        res.redirect('/route/dashboard')
     }else{
-        res.end('Invalid username')
+        res.end('Invalid credentials :(')
     }
+})
+
+//Dashboard router
+//If there is information for the user, render dashboard
+//Else, print error
+router.get('/dashboard', (req, res) => {
+    if(req.session.user){
+        res.render(('dashboard'), { user: req.session.user })
+    }else{
+        res.send('Unauthorized User :(')
+    }
+})
+
+//Logout
+//Destroy the session variable of the logged in user
+router.get('/logout', (req, res) => {
+    req.session.destroy(function(err){
+        if(err){
+            res.send('Error')
+        }else{
+            res.render('base', { title: 'Login System', logout: 'Logout successful' })
+        }
+    })
 })
 
 
--- server.js
+++ server.js
@@ -2,17 +2,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+
 const bodyparser = require('body-parser')
 const session = require('express-session')
 const { v4: uuidv4 } = require('uuid')
 const router = require('./router.js')
 
+
 //Setup ejs engine
 app.set('view engine', 'ejs')
-
-//Setup body-parser
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: true }))
 
 //Setup express session
 //Will generate an unique id for every session and keep it private
@@ -22,15 +20,23 @@ app.use(session({
     saveUninitialized: true
 }))
 
-//Setup router middleware
-app.use('/route', router)
+//Setup body-parser
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
 
-//Home router
+
+
+
+//Home route on server start
 app.get('/', (req, res) => {
     res.render('base', { title: 'Login System' })
 })
 
+//Setup router middleware
+//Errors out if this is above express-session and body-parser
+app.use('/route', router)
+
 //Listen for server connection @ port variable
 app.listen(port, () => {
-    console.log('Connected to server!')
+    console.log(`Connected to server at port ${port}!`)
 })
